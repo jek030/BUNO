6935f9a8c294bba5ce8a57f445def079f45169ae
Lily Romano <nrr004@bucknell.edu>
2018-11-09 16:31:43 -0500

    Refactor DeckTest.java

--- test/cards/DeckTest.java
+++ test/cards/DeckTest.java
@@ -21,20 +21,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Tests the Deck class
  *
- * @author lilyheart
+ * @author Lily Romano
  */
 public class DeckTest {
 
-    public DeckTest() {
-    }
+    Deck newDeck;
+    String defaultDeckString = "[Card{color=BLUE, type=ZERO}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=SKIP}, Card{color=BLUE, type=SKIP}, Card{color=ORANGE, type=ZERO}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=SKIP}, Card{color=ORANGE, type=SKIP}, Card{color=YELLOW, type=ZERO}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=SKIP}, Card{color=YELLOW, type=SKIP}, Card{color=GRAY, type=ZERO}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=SKIP}, Card{color=GRAY, type=SKIP}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}]";
 
+    /**
+     * Creates a new deck before every individual test
+     */
     @Before
     public void setUp() {
+        newDeck = new Deck();
     }
 
+    /**
+     * Deletes the deck before every individual test
+     */
     @After
     public void tearDown() {
+        newDeck = null;
     }
 
     /**
@@ -45,42 +54,38 @@ public class DeckTest {
     public void testFullDeck() {
         System.out.println("testFullDeck");
 
-        String expectedDeckString = "[Card{color=BLUE, type=ZERO}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=SKIP}, Card{color=BLUE, type=SKIP}, Card{color=ORANGE, type=ZERO}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=SKIP}, Card{color=ORANGE, type=SKIP}, Card{color=YELLOW, type=ZERO}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=SKIP}, Card{color=YELLOW, type=SKIP}, Card{color=GRAY, type=ZERO}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=SKIP}, Card{color=GRAY, type=SKIP}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}]";
-        Deck newDeck = new Deck();
         String newDeckString = newDeck.getDeck().toString();
 
-        if (!expectedDeckString.equals(newDeckString)) {
-            String errorMessage = "Expected: " + expectedDeckString + "\nActual: " + newDeckString;
+        if (!defaultDeckString.equals(newDeckString)) {
+            String errorMessage = "Expected: " + defaultDeckString + "\nActual: " + newDeckString;
             System.out.println(errorMessage);
         }
 
-        assertEquals(expectedDeckString, newDeckString);
+        assertEquals(defaultDeckString, newDeckString);
     }
 
     /**
-     * Test of main method, of class Deck.
+     * Test the shuffle method
      */
     @Test
     public void testShuffle() {
         System.out.println("testShuffle");
 
-        String unshuffledDeckString = "[Card{color=BLUE, type=ZERO}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=ONE}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=TWO}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=THREE}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FOUR}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=FIVE}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SIX}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=SEVEN}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=EIGHT}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=NINE}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=DRAW2}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=REVERSE}, Card{color=BLUE, type=SKIP}, Card{color=BLUE, type=SKIP}, Card{color=ORANGE, type=ZERO}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=ONE}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=TWO}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=THREE}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FOUR}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=FIVE}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SIX}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=SEVEN}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=EIGHT}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=NINE}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=DRAW2}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=REVERSE}, Card{color=ORANGE, type=SKIP}, Card{color=ORANGE, type=SKIP}, Card{color=YELLOW, type=ZERO}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=ONE}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=TWO}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=THREE}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FOUR}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=FIVE}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SIX}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=SEVEN}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=EIGHT}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=NINE}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=DRAW2}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=REVERSE}, Card{color=YELLOW, type=SKIP}, Card{color=YELLOW, type=SKIP}, Card{color=GRAY, type=ZERO}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=ONE}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=TWO}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=THREE}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FOUR}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=FIVE}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SIX}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=SEVEN}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=EIGHT}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=NINE}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=DRAW2}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=REVERSE}, Card{color=GRAY, type=SKIP}, Card{color=GRAY, type=SKIP}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILD}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}, Card{color=SPECIAL, type=WILDDRAW4}]";
-        Deck newDeck = new Deck();
         String newDeckString = newDeck.getDeck().toString();
 
-        //Ensure unshuffed deck is correct, otherwise asserting false would pass even if wrong
-        assertEquals(unshuffledDeckString, newDeckString);
+        //Ensure unshuffed deck is correct, otherwise asserting false after shuffle would pass even if shuffle was not working
+        assertEquals(defaultDeckString, newDeckString);
 
         //Shuffle and assert
         newDeck.shuffle();
         String newShuffledDeckString = newDeck.getDeck().toString();
 
-        if (unshuffledDeckString.equals(newShuffledDeckString)) {
-            String errorMessage = "Unshuffled: " + unshuffledDeckString + "\nActual: " + newShuffledDeckString;
+        if (defaultDeckString.equals(newShuffledDeckString)) {
+            String errorMessage = "Unshuffled: " + defaultDeckString + "\nActual: " + newShuffledDeckString;
             System.out.println(errorMessage);
         }
 
-        assertFalse(unshuffledDeckString.equals(newShuffledDeckString));
+        assertFalse(defaultDeckString.equals(newShuffledDeckString));
     }
 
 }
