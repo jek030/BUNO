5c8dd245600c0ef6f7ece3b00b94d819fd27be5b
Lily Romano <nrr004@bucknell.edu>
2018-11-09 12:30:31 -0500

    Add point values to card types

--- src/cards/CardType.java
+++ src/cards/CardType.java
@@ -24,63 +24,63 @@ public enum CardType {
     /**
      * An enum with information on the card zero
      */
-    ZERO("0"),
+    ZERO("0", 0),
     /**
      * An enum with information on the card one
      */
-    ONE("1"),
+    ONE("1", 1),
     /**
      * An enum with information on the card two
      */
-    TWO("2"),
+    TWO("2", 2),
     /**
      * An enum with information on the card three
      */
-    THREE("3"),
+    THREE("3", 3),
     /**
      * An enum with information on the card four
      */
-    FOUR("4"),
+    FOUR("4", 4),
     /**
      * An enum with information on the card five
      */
-    FIVE("5"),
+    FIVE("5", 5),
     /**
      * An enum with information on the card six
      */
-    SIX("6"),
+    SIX("6", 6),
     /**
      * An enum with information on the card seven
      */
-    SEVEN("7"),
+    SEVEN("7", 7),
     /**
      * An enum with information on the card eight
      */
-    EIGHT("8"),
+    EIGHT("8", 8),
     /**
      * An enum with information on the card nine
      */
-    NINE("9"),
+    NINE("9", 9),
     /**
      * An enum with information on the card draw 2
      */
-    DRAW2("+2"),
+    DRAW2("+2", 20),
     /**
      * An enum with information on the card reverse
      */
-    REVERSE("⇆"),
+    REVERSE("⇆", 20),
     /**
      * An enum with information on the card skip
      */
-    SKIP("Ø"),
+    SKIP("Ø", 20),
     /**
      * An enum with information on the card wild
      */
-    WILD("Wild"),
+    WILD("Wild", 50),
     /**
      * An enum with information on the card wild draw 4
      */
-    WILDDRAW4("WildDraw4");
+    WILDDRAW4("WildDraw4", 50);
 
     /**
      * A String that represents the card type
@@ -88,12 +88,18 @@ public enum CardType {
     private final String cardText;
 
     /**
+     * The scoring value of the card type
+     */
+    private final int cardValue;
+
+    /**
      * An explicit constructor for the Card Type enumerated type
      *
      * @param cardText a {@code String} representing the card type
      */
-    private CardType(String cardText) {
+    private CardType(String cardText, int cardValue) {
         this.cardText = cardText;
+        this.cardValue = cardValue;
     }
 
     /**
