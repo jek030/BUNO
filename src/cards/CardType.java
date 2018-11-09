798c0879c075cdd451673e2339b647784eb9e043
Lily Romano <nrr004@bucknell.edu>
2018-11-09 11:54:32 -0500

    Add enum of CardType

---
+++ src/cards/CardType.java
@@ -0,0 +1,108 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2018
+*
+* Name: James Kelly, Scott Little, Rachel Wang, Lily Romano
+* Date: Nov 9, 2018
+* Time: 11:30:29 AM
+*
+* Project: csci205FinalProject
+* Package: cards
+* File: CardType
+* Description:
+*
+* ****************************************
+ */
+package cards;
+
+/**
+ *
+ * @author Lily Romano
+ */
+public enum CardType {
+
+    /**
+     * An enum with information on the card zero
+     */
+    ZERO("0"),
+    /**
+     * An enum with information on the card one
+     */
+    ONE("1"),
+    /**
+     * An enum with information on the card two
+     */
+    TWO("2"),
+    /**
+     * An enum with information on the card three
+     */
+    THREE("3"),
+    /**
+     * An enum with information on the card four
+     */
+    FOUR("4"),
+    /**
+     * An enum with information on the card five
+     */
+    FIVE("5"),
+    /**
+     * An enum with information on the card six
+     */
+    SIX("6"),
+    /**
+     * An enum with information on the card seven
+     */
+    SEVEN("7"),
+    /**
+     * An enum with information on the card eight
+     */
+    EIGHT("8"),
+    /**
+     * An enum with information on the card nine
+     */
+    NINE("9"),
+    /**
+     * An enum with information on the card draw 2
+     */
+    DRAW2("+2"),
+    /**
+     * An enum with information on the card reverse
+     */
+    REVERSE("⇆"),
+    /**
+     * An enum with information on the card skip
+     */
+    SKIP("Ø"),
+    /**
+     * An enum with information on the card wild
+     */
+    WILD("Wild"),
+    /**
+     * An enum with information on the card wild draw 4
+     */
+    WILDDRAW4("WildDraw4");
+
+    /**
+     * A String that represents the card type
+     */
+    private final String cardText;
+
+    /**
+     * An explicit constructor for the Card Type enumerated type
+     *
+     * @param cardText a {@code String} representing the card type
+     */
+    private CardType(String cardText) {
+        this.cardText = cardText;
+    }
+
+    /**
+     * Returns the string representation of the card type
+     *
+     * @return the string representation of the card type
+     */
+    public String getCardText() {
+        return cardText;
+    }
+
+}
